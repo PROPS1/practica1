ab215c4729e500204ffd47e2006bb496b6c7a549
PROPS12 <chichagovd@yandex.ru>
2020-09-08 14:12:21 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace practica1
+{
+  class Program
+  {
+    struct person
+    {
+      public string firstname;
+      public string secondname;
+      int age;
+    public person(string _firstname,string _secondname,int _age)
+      {
+        firstname = _firstname;
+        secondname = _secondname;
+        age = _age;
+      }
+     public override string ToString()
+    {
+      return firstname + " "+ secondname +" "+ age;
+    }
+   }
+    static void Main(string[] args) {
+
+    person p = new person("Tony", "Allen", 32);
+    GC.Collect();
+    Console.WriteLine(p);
+  }
+ }
+}
+
